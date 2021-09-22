1db8cf4f2ee279aff9a9c93d3b675408df82d2d4
PaolaPedraza07 <paolapedraza.3007@gmail.com>
2021-09-21 21:19:57 -0500

    cambios contact us

--- javascript/contactanos.js
+++ javascript/contactanos.js
@@ -1,5 +1,16 @@
-var input = document.getElementById('celular');
+let input = document.getElementById('celular');
 input.oninvalid = function(event) {
   event.target.setCustomValidity('Ingrese un numero de 10 dígitos');  
+}
+
+function enviarCorreo(){
+  let nombre = document.getElementById("nombre").value;
+  let correo = document.getElementById("correo").value;
+  let celular = document.getElementById("celular").value;
+  let mensaje = document.getElementById("mensaje").value;
+  console.log(nombre, correo, celular, mensaje)
+
+  alert(" ");
+
 }
 
