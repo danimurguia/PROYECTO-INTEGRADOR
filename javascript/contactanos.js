b775b82e852751a9a820067ee1c71211c96cad2f
PaolaPedraza07 <paolapedraza.3007@gmail.com>
2021-09-22 22:28:32 -0500

    Agregando funcionalidad a formulario

--- javascript/contactanos.js
+++ javascript/contactanos.js
@@ -8,9 +8,24 @@ function enviarCorreo(){
   let correo = document.getElementById("correo").value;
   let celular = document.getElementById("celular").value;
   let mensaje = document.getElementById("mensaje").value;
-  console.log(nombre, correo, celular, mensaje)
 
-  alert(" ");
-
+  
+  Email.send({
+    SecureToken : "178c9477-67d5-48bc-b69a-2058032089f3",
+    From : correo,
+    To : 'paolapedraza.3007@gmail.com',
+    
+    Subject : "Mensaje de SporTech",
+    Body : "Nombre: "+nombre+"<br>Correo: "+correo+"<br>Telefono: "+celular+"<br>Mensaje: "+mensaje
+  }).then( (message) => {
+    let p;
+    if(message!="OK"){
+      p = document.getElementById('msgEmailError');
+      p.removeAttribute("hidden")   
+    }else{
+      console.log(message)
+      p = document.getElementById('msgEmail');
+      p.removeAttribute("hidden") 
+    }   
+  })
 }
-
