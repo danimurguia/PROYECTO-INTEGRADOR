3bbf075a6a29c68f57d6f4020d8924b7690fff91
PaolaPedraza07 <paolapedraza.3007@gmail.com>
2021-10-28 11:26:46 -0500

    Inicio sesion, y página principal

--- javascript/inicioSesion.js
+++ javascript/inicioSesion.js
@@ -1,3 +1,8 @@
+//Si la fecha expiracion es mayor a nuestra fecha actual es por que aún hay sesión
+if(jwtIsActive()){
+    document.location.href="/html/paginaPrincipal.html";
+}
+
 //Ejecutando funciones
 document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
 document.getElementById("btn__registrarse").addEventListener("click", register);
@@ -18,98 +23,99 @@ var caja_trasera_register = document.querySelector(".caja__trasera-register");
 let registros = [];
 
 
-        // example {id:1592304983049, title: 'Deadpool', year: 2015}
-        const addRegistro = (ev)=>{
-            ev.preventDefault();  //to stop the form submitting
-            
-              
-            let registro= {
-                id: Date.now(),
-                nombreRegistro: document.getElementById('nombreRegistro').value,
-                correoRegistro: document.getElementById('correoRegistro').value,
-                contraseñaRegistro: document.getElementById('contraseñaRegistro').value,
-                contraseñaRegistro2: document.getElementById('contraseñaRegistro2').value
-                
-                   
-            }
-
-            
-            let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-            let correoValido = expReg.test(correoRegistro.value)
-            let expReg2 = /^(?=.*[a-z])(?=.*\d)([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
-            let contraseñaValido = expReg2.test(contraseñaRegistro.value)
-            
-            //saving to localStorage
-            if(nombreRegistro.value != "" && correoValido == true  &&contraseñaValido == true && contraseñaRegistro.value == contraseñaRegistro2.value) 
-            {
-                
-                registros.push(registro);
-                //document.forms[0].reset(); 
-                
+// example {id:1592304983049, title: 'Deadpool', year: 2015}
+const addRegistro = async (ev)=>{
+    ev.preventDefault();  //to stop the form submitting
     
-                //for display purposes only
-                console.warn('Usuario Agregado al Local Storage' , {registros} );
-                localStorage.setItem('Usuarios', JSON.stringify(registros) );
+    let registro= {
+        nombreRegistro: document.getElementById('nombreRegistro').value,
+        correoRegistro: document.getElementById('correoRegistro').value,
+        contraseñaRegistro: document.getElementById('contraseñaRegistro').value,
+        contraseñaRegistro2: document.getElementById('contraseñaRegistro2').value
+    }
+
+    
+    let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    let correoValido = expReg.test(correoRegistro.value)
+    let expReg2 = /^(?=.*[a-z])(?=.*\d)([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+    let contraseñaValido = expReg2.test(contraseñaRegistro.value)
+    
+    //saving to localStorage
+    if(nombreRegistro.value != "" && correoValido == true  &&contraseñaValido == true && contraseñaRegistro.value == contraseñaRegistro2.value) 
+    {
+        try {
+            const res = await fetch('http://localhost:8080/cliente/sign-in',  {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                    {
+                        "nombre": registro.nombreRegistro,
+                        "correo": registro.correoRegistro,
+                        "contrasenia": registro.contraseñaRegistro
+                    }
+                ) 
+            })
+    
+            const statusCode = await res.status;
+            const result = await res.text() //respuesta del endpoint(jwtToken)
+    
+            if(statusCode === 200){
+                console.log(result)
                 Swal.fire(
                     '¡Ya estás registrado!',
                     'Ahora podrás recibir nuestras promociones, descuentos especiales y ¡mucho más!',
                     'success'
-                  )
+                )
                 document.querySelector('.formulario__register').reset();// to clear the form for the next entries
-            
-                } else if ( correoValido == false ) {
-               
+            }else{
+                console.log(result)
                 Swal.fire({
                     icon: 'error',
                     title: 'Oops...',
-                    text:  'Revisa el formato del correo: correo@ejemplo.com' 
-                   
-                  })
-                  document.getElementById('correoRegistro').value = ''// to clear the input for the next
-            } else if ( contraseñaValido == false ) {
-               
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text:  'Revisa el formato de contraseña: min. 8 caracteres, 1 número, sin espacios' 
-                   
-                  })
-                  document.getElementById('contraseñaRegistro').value = ''// to clear the form for the next
-            } else if (  contraseñaRegistro.value != contraseñaRegistro2.value) {
-               
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text:  'Las contraseñas no coinciden' 
-                   
-                  })
-                  document.getElementById('contraseñaRegistro2').value = ''// to clear the form for the next
+                    text: result 
+                })
             }
-            
-            else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text:  'Completa los campos' 
-                   
-                  })
-            }
-            
-            }
-       
+        }catch (error){
+            console.log(error)
+        }
+    
+    } else if ( correoValido == false ) {   
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text:  'Revisa el formato del correo: correo@ejemplo.com' 
+        })
+        document.getElementById('correoRegistro').value = ''// to clear the input for the next
+    } else if ( contraseñaValido == false ) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text:  'Revisa el formato de contraseña: min. 8 caracteres, 1 número, sin espacios' 
+        })
+        document.getElementById('contraseñaRegistro').value = ''// to clear the form for the next
+    } else if (  contraseñaRegistro.value != contraseñaRegistro2.value) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text:  'Las contraseñas no coinciden' 
+        })
+        document.getElementById('contraseñaRegistro2').value = ''// to clear the form for the next
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text:  'Completa los campos'    
+        })
+    }
+    
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById("btnRegistro").addEventListener("click", addRegistro);
+});
 
-        
-        
-        document.addEventListener('DOMContentLoaded', ()=>{
-        document.getElementById("btnRegistro").addEventListener("click", addRegistro)
-        ;});
-        
-        
-
-   
-
-    //FUNCIONES
-
+//FUNCIONES
 function anchoPage(){
 
     if (window.innerWidth > 850){
@@ -170,5 +176,45 @@ function mostrarContrasena(){
     }else{
         tipo.type = "password";
         tipo2.type = "password";
+    }
+}
+
+async function logInCliente(){
+    let correo = document.getElementById("correoLogIn").value;
+    let contrasenia = document.getElementById("contraseniaLogIn").value;
+    
+    try {
+        const res = await fetch('http://localhost:8080/login-cliente',  {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    "correo":correo,
+                    "contrasenia":contrasenia
+                }
+            ) 
+        })
+
+        const statusCode = await res.status;
+        const result = await res.text() //respuesta del endpoint(jwtToken)
+
+        if(statusCode === 200){
+            console.log(result)
+            localStorage.setItem('jwt-token', result);
+            //redirect to index
+            document.location.href="/html/paginaPrincipal.html";
+        }else{
+            localStorage.setItem('jwt-token', "" );
+            console.log(result)
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: result 
+            })
+        }
+    }catch (error){
+        console.log(error)
     }
 }
