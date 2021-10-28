e2c59b0c32417c6d7d0a44ea78db5510a0eee7be
Jocelyne28 <jocy_alch@hotmail.com>
2021-10-28 11:18:57 -0500

    Agregar prod a carrito

--- javascript/articulosCargar.js
+++ javascript/articulosCargar.js
@@ -1,14 +1,19 @@
 const cards = document.getElementById('cards');
 const templateCard = document.getElementById('template-card-accesorios').content
 const fragment = document.createDocumentFragment()
+let carrito = {};
 
-document.addEventListener('DOMContentLoaded', () => {fetchData()
- } )
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+    if(localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+    }
+});
 
 // Traer productos
 const fetchData = async () => {
     try {
-    const res = await fetch('http://localhost:8080/productos')
+    const res = await fetch('http://192.168.2.101:8080/productos')
     const data = await res.json()
     //console.log(data)
     pintarCards(data)
@@ -20,16 +25,42 @@ const fetchData = async () => {
 const pintarCards = data => {
     data.forEach(producto => {
         if(producto.categoria == "Accesorios"){
-        templateCard.querySelector('h5').textContent = producto.nombre
-        templateCard.querySelector('p').textContent = "$" + producto.precio
-        templateCard.querySelector('.p2').textContent = producto.marca
-        templateCard.querySelector('img').setAttribute("src", producto.archivo)
-        templateCard.querySelector('.btn-dark').dataset.id = producto.id
-        
-        
-        const clone = templateCard.cloneNode(true)
-        fragment.appendChild(clone)
+            templateCard.querySelector('h5').textContent = producto.nombre;
+            templateCard.querySelector('p').textContent = "$" + producto.precio;
+            templateCard.querySelector('.p2').textContent = producto.marca
+            templateCard.querySelector('img').setAttribute("src", producto.archivo);
+            templateCard.querySelector('.btn-dark').dataset.id = producto.id;
+            
+            const clone = templateCard.cloneNode(true)
+            fragment.appendChild(clone)
         }
     })
     cards.appendChild(fragment)
+}
+
+const addToCart = (element) => {
+    if(element.classList.contains('btn-dark')){
+        setCarrito(element.parentElement)
     }
+};
+
+const setCarrito = objeto => {
+    const precio = parseFloat(objeto.querySelector('p').textContent.split("$")[1]);
+    const title = objeto.querySelector('h5').textContent;
+    const brand = objeto.querySelector('.p2').textContent;
+    const producto = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        title: `${title} (${brand})`,
+        precio: precio,
+        cantidad: 1
+    }
+
+    if(carrito.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito[producto.id].cantidad +1
+    }
+    carrito[producto.id] = {...producto}
+    //pintarCarrito()
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+    document.querySelector('.alert span').textContent = `${title} agregado al carrito`
+    $('.alert').show();
+}
